3c34d642caa9d0cae7233236844c2a753eceb338
Carlos Trevisan <carlosedutrevisan@gmail.com>
2026-03-29 17:57:45 -0400

    Add Clerk appearance config to provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,9 +5,19 @@ import App from "./App.jsx";
 import "./index.css";
 import "leaflet/dist/leaflet.css";
 
+const clerkAppearance = {
+  layout: {
+    unsafe_disableDevelopmentModeWarnings: false,
+  },
+  elements: {
+    card: "w-full max-w-[calc(100vw-2rem)] sm:max-w-md mx-auto",
+    rootBox: "w-full",
+  },
+};
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ClerkProvider afterSignOutUrl="/">
+    <ClerkProvider afterSignOutUrl="/" appearance={clerkAppearance}>
       <App />
     </ClerkProvider>
   </React.StrictMode>
